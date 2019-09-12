4860f590e7ab00ef87d2c70b580f89f101651561
Casdegraaff <54799365+Casdegraaff@users.noreply.github.com>
2019-09-12 12:02:30 +0200

    Add files via upload

---
+++ opdracht 1  12.9.2019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pr02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-----------------  De game des games-------------------");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
