7dabea2bd0208dcb39cea038e82bd1054144f88e
Casdegraaff <54799365+Casdegraaff@users.noreply.github.com>
2019-10-10 12:07:31 +0200

    Githubbertje

---
+++ Githubbertje/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Githubbertje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("he, Waar ben ik nu weer heen gepushed!");
+        }
+    }
+}
